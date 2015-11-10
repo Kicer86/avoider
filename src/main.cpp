c054def5ce17b2efec4a0fc1282b49ce8d0c86ad
Michał Walenciak <Kicer86@gmail.com>
2015-11-10 23:07:56 +0100

    main: set resize mode to SizeRootObjectToView
    This forces top item to follow screen size

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQuickView view;
+    view.setResizeMode(QQuickView::SizeRootObjectToView);
     view.setSource(QUrl("qrc:/qml/avoider.qml"));
     view.show();
 
