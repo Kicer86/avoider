02db6fda43def5c00c9b856ec2dd084da654365b
Michał Walenciak <Kicer86@gmail.com>
2015-11-30 19:29:25 +0100

    "Loading..." splash screen for android

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,35 @@
 
 #include <QApplication>
+#include <QPainter>
 #include <QQuickWidget>
-
+#include <QScreen>
 #include <QSplashScreen>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    QQuickWidget view;
 
-    QPixmap pixmap(":/img/background.jpg");
+#if defined(Q_OS_ANDROID)
+
+    QScreen* screen = app.primaryScreen();
+    QRect geometry = screen->geometry();
+
+    QPixmap pixmap(geometry.size());
+    pixmap.fill(Qt::black);
+
+    QPainter painter(&pixmap);
+    painter.setPen(Qt::white);
+    painter.setFont(QFont("Arial", 20));
+    painter.drawText(geometry, Qt::AlignCenter, QObject::tr("Loading..."));
+
     QSplashScreen splash(pixmap);
     splash.show();
     app.processEvents();
 
-    QQuickWidget view;
     splash.finish(&view);
+#endif
+
     view.setResizeMode(QQuickWidget::SizeRootObjectToView);
     view.setSource(QUrl("qrc:/qml/avoider.qml"));
     view.show();
