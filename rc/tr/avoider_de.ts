4b19caa15bdebb96bf3b22424ddbe05c781d934b
Wojciech Kucjas <mail2kaceka@gmail.com>
2015-12-13 14:44:05 +0100

    Adding TranslationChange class

---
+++ rc/tr/avoider_de.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>avoider</name>
+    <message>
+        <location filename="../qml/avoider.qml" line="196"/>
+        <source>Begin game</source>
+        <translation>Spiel beginnen</translation>
+    </message>
+    <message>
+        <location filename="../qml/avoider.qml" line="210"/>
+        <source>GAME OVER</source>
+        <translation>Spiel ist aus</translation>
+    </message>
+</context>
+</TS>
--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,60 @@
 #include <QScreen>
 #include <QSplashScreen>
 #include <QTranslator>
+#include <QString>
+#include <QQmlContext>
+
+class TranslationChange : public QObject{
+ Q_OBJECT
+ Q_PROPERTY(QString emptyString READ getEmptyString NOTIFY languageChanged)
+
+   QString getEmptyString() {
+   return "";
+  }
+
+
+
+   Q_INVOKABLE void selectLanguage(QString language) {
+       QTranslator translator;
+
+       if(language == QString("fr")) {
+      translator.load("avoider_fr", ":/tr");    // Loading french translation
+      qApp->installTranslator(&translator);
+     }
+
+     if(language == QString("de")) {
+      translator.load("avoider_de", ":/tr");    // Loading german translation
+      qApp->installTranslator(&translator);
+     }
+
+     if(language == QString("en")) {
+      qApp->removeTranslator(&translator);
+     }
+
+     emit languageChanged();
+    }
+
+    signals:
+     void languageChanged();
+};
+
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     QQuickWidget view;
 
-    QTranslator translator;
 
-    const bool load = translator.load("avoider_pl", ":/tr");
-    const bool install = app.installTranslator(&translator);
+    TranslationChange myObj;
+//    QTranslator translator;
 
-    assert(load && install);
+//    const bool load = translator.load("avoider_pl", ":/tr");    // Loading polish translation
+//    const bool load2 = translator.load("avoider_de", ":/tr");    // Loading german translation
+//    const bool load3 = translator.load("avoider_fr", ":/tr");    // Loading french translation
+
+//    const bool install = app.installTranslator(&translator);
+
+//    assert(load && install);
 
 #if defined(Q_OS_ANDROID)
 
@@ -39,7 +81,7 @@ int main(int argc, char *argv[])
 
     splash.finish(&view);
 #endif
-
+    view.rootContext()->setContextProperty("rootItem", (QObject*)&myObj); //tr
     view.setResizeMode(QQuickWidget::SizeRootObjectToView);
     view.setSource(QUrl("qrc:/qml/avoider.qml"));
     view.show();
