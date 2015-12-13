b26bd262a8afbc3cb5c324b1e8d19b62596dbaf9
Wojciech Kucjas <mail2kaceka@gmail.com>
2015-12-13 17:04:43 +0100

    Approach changed. Added working locale switcher.

--- src/main.cpp
+++ src/main.cpp
@@ -8,59 +8,43 @@
 #include <QSplashScreen>
 #include <QTranslator>
 #include <QString>
-#include <QQmlContext>
 
-class TranslationChange : public QObject{
- Q_OBJECT
- Q_PROPERTY(QString emptyString READ getEmptyString NOTIFY languageChanged)
-
-   QString getEmptyString() {
-   return "";
-  }
-
-
-
-   Q_INVOKABLE void selectLanguage(QString language) {
-       QTranslator translator;
-
-       if(language == QString("fr")) {
-      translator.load("avoider_fr", ":/tr");    // Loading french translation
-      qApp->installTranslator(&translator);
-     }
-
-     if(language == QString("de")) {
-      translator.load("avoider_de", ":/tr");    // Loading german translation
-      qApp->installTranslator(&translator);
-     }
-
-     if(language == QString("en")) {
-      qApp->removeTranslator(&translator);
-     }
-
-     emit languageChanged();
-    }
-
-    signals:
-     void languageChanged();
-};
 
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QQuickWidget view;
+    QQuickWidget view;    
+
+    QString locale = QLocale::system().name();  // Reading locale settings
+
+    QTranslator translator;
+
+// Locale switcher
+
+    if (locale == "pl_PL")
+    {
+      const bool load = translator.load("avoider_pl", ":/tr");    // Loading polish translation
+      const bool install = app.installTranslator(&translator);
+          assert(load && install);    // Assert for loading translation
+    }else if (locale == "de_DE")
+    {
+      const bool load = translator.load("avoider_de", ":/tr");    // Loading german translation
+      const bool install = app.installTranslator(&translator);
+          assert(load && install);    // Assert for loading translation
+    }else if (locale == "fr_FR")
+    {
+      const bool load = translator.load("avoider_fr", ":/tr");    // Loading french translation
+      const bool install = app.installTranslator(&translator);
+          assert(load && install);    // Assert for loading translation
+    }else
+    {
+      const bool load = true;
+      const bool install = true;
+          assert(load && install);    // Assert for loading translation
+    }
 
 
-    TranslationChange myObj;
-//    QTranslator translator;
-
-//    const bool load = translator.load("avoider_pl", ":/tr");    // Loading polish translation
-//    const bool load2 = translator.load("avoider_de", ":/tr");    // Loading german translation
-//    const bool load3 = translator.load("avoider_fr", ":/tr");    // Loading french translation
-
-//    const bool install = app.installTranslator(&translator);
-
-//    assert(load && install);
 
 #if defined(Q_OS_ANDROID)
 
@@ -81,7 +65,7 @@ int main(int argc, char *argv[])
 
     splash.finish(&view);
 #endif
-    view.rootContext()->setContextProperty("rootItem", (QObject*)&myObj); //tr
+
     view.setResizeMode(QQuickWidget::SizeRootObjectToView);
     view.setSource(QUrl("qrc:/qml/avoider.qml"));
     view.show();
